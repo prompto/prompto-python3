6bad622dd13883ad7c50d1229a5dcb1ad4688c31
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-05 18:24:11 +0800

    fix install warnings + typo in test script

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
-echo "begin script"
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/generated
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/main
 nosetests Python3-Core/src/test/prompto/
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Runtime/src/main
-$export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/test
-nosetests /Python3-Runtime/src/test/
-echo "end script"
+export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/test
+nosetests Python3-Runtime/src/test/
