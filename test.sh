7ae94371d07230c8e34a7f15a52e0a49743aea6f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-05 18:31:00 +0800

    not all tests were executed

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/generated
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/main
-nosetests Python3-Core/src/test/prompto/
+nosetests --exe Python3-Core/src/test/prompto/
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Runtime/src/main
 export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/test
-nosetests Python3-Runtime/src/test/
+nosetests --exe Python3-Runtime/src/test/
