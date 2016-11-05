628878d8938910b4a4f1d458b3d08db19ae0f399
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2016-11-05 18:12:34 +0800

    Travis CI next step

--- test.sh
+++ test.sh
@@ -1,1 +1,8 @@
-echo "script ok"
+echo "begin script"
+export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/generated
+export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/main
+nosetests Python3-Core/src/test/prompto/
+export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Runtime/src/main
+$export PYTHONPATH=$PYTHONPATH:$TRAVIS_BUILD_DIR/Python3-Core/src/test
+nosetests /Python3-Runtime/src/test/
+echo "end script"
