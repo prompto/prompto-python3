dac0119a842d1dfee9236fbd8c08813ac371f52d
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-08-08 00:17:41 +0800

    support document literal syntax

---
+++ Python3-Core/src/main/prompto/literal/DocEntryList.py
@@ -0,0 +1,35 @@
+from io import StringIO
+
+class DictEntryList ( list ):
+
+    def __init__(self, entry = None):
+        super(DictEntryList, self).__init__()
+        if entry is not None:
+            self.append(entry)
+
+    def __str__(self):
+        with StringIO() as sb:
+            sb.write('<')
+            for item in self:
+                sb.write(str(item))
+                sb.write(", ")
+            slen = sb.tell()
+            if slen>2:
+                sb.truncate(slen-2)
+                sb.seek(slen-2)
+            else:
+                sb.write(':')
+            sb.write('>')
+            return sb.getvalue()
+
+
+    def toDialect(self, writer):
+        writer.append('<')
+        if len(self)>0:
+            for entry in self:
+                entry.toDialect(writer)
+                writer.append(", ")
+            writer.trimLast(2)
+        else:
+            writer.append(':')
+        writer.append('>')
---
+++ Python3-Core/src/main/prompto/literal/DocumentLiteral.py
@@ -0,0 +1,58 @@
+from prompto.literal.DictEntryList import DictEntryList
+from prompto.literal.Literal import Literal
+from prompto.type.MissingType import MissingType
+from prompto.type.TextType import TextType
+from prompto.value.Dictionary import Dictionary
+from prompto.error.SyntaxError import SyntaxError
+from prompto.utils.TypeUtils import inferElementType
+
+class DictLiteral(Literal):
+    # we can only compute keys by evaluating key expressions
+    # so we can't just inherit from dict
+    # so we keep the full entry list.
+    def __init__(self, mutable, entries=None):
+        if entries is None:
+            entries = DictEntryList()
+        super().__init__("<:>", Dictionary(MissingType.instance, mutable))
+        self.mutable = mutable
+        self.entries = entries
+        self.itemType = None
+
+
+    def toDialect(self, writer):
+        if self.mutable:
+            writer.append("mutable ")
+        self.entries.toDialect(writer)
+
+
+    def check(self, context):
+        from prompto.type.DictType import DictType
+        if self.itemType is None:
+            self.itemType = self.inferElementType(context)
+        return DictType(self.itemType)
+
+
+
+    def inferElementType(self, context):
+        if len(self.entries) == 0:
+            return MissingType.instance
+        keyTypes = [e.getKey().check(context) for e in self.entries]
+        for keyType in keyTypes:
+            if keyType != TextType.instance:
+                raise SyntaxError("Illegal key type: " + keyType.toString())
+        expressions = [e.getValue() for e in self.entries]
+        return inferElementType(context, expressions)
+
+
+
+    def interpret(self, context):
+        if len(self.entries) > 0:
+            self.check(context)
+            value = dict()
+            for e in self.entries:
+                key = e.getKey().interpret(context)
+                val = e.getValue().interpret(context)
+                value[key.value] = val
+            return Dictionary(self.itemType, self.mutable, value=value)
+        else:
+            return self.value
