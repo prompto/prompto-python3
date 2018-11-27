2625ca2b89a8c47aefde4ab3daa4e08ba50d21b2
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-11-28 01:09:38 +0800

    fix failing tests

--- Python3-Core/src/main/prompto/value/DateTime.py
+++ Python3-Core/src/main/prompto/value/DateTime.py
@@ -117,3 +117,42 @@ class DateTime(BaseValue):
 
     def __hash__(self):
         return hash(self.value)
+
+
+    @staticmethod
+    def Parse(text):
+        year = int(text[0:4])
+        text = text[5:]
+        month = int(text[0:2])
+        text = text[3:]
+        day = int(text[0:2])
+        text = text[3:]
+        if len(text) > 0:
+            hour = int(text[0:2])
+            text = text[3:]
+        else:
+            hour = 0
+        if len(text) > 0:
+            minute = int(text[0:2])
+            text = text[3:]
+        else:
+            minute = 0
+        if len(text) > 0:
+            second = int(text[0:2])
+            text = text[2:]
+        else:
+            second = 0
+        if len(text) > 0 and text[0] == '.':
+            text = text[1:]
+            buffer = ""
+            while len(text) > 0 and text[0] >= '0' and text[0] <= '9':
+                buffer += text[0]
+                text = text[1:]
+            while len(buffer) < 6:
+                buffer += '0'
+            micro = int(buffer)
+        else:
+            micro = 0
+        zone = None
+        dt = datetime(year, month, day, hour, minute, second, micro, zone)
+        return DateTime(value=dt)
