a48cb513a0a6b3e60adc069c77619521e2fad7bd
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-11-25 14:17:12 +0800

    upgrade antlr4 to 4.9

--- Python3-Core/src/generated/prompto/parser/OParser.py
+++ Python3-Core/src/generated/prompto/parser/OParser.py
@@ -1,4 +1,4 @@
-# Generated from OParser.g4 by ANTLR 4.8
+# Generated from OParser.g4 by ANTLR 4.9
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -2192,7 +2192,7 @@ class OParser ( AbstractParser ):
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.8")
+        self.checkVersion("4.9")
         self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
         self._predicates = None
 
