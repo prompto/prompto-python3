9746c5df2a5913ab3472c22fdf6d56c9e7ba48bd
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-12-23 17:27:10 +0800

    missing abstract method

--- Python3-Core/src/main/prompto/expression/PredicateExpression.py
+++ Python3-Core/src/main/prompto/expression/PredicateExpression.py
@@ -2,3 +2,6 @@ class PredicateExpression(object):
 
     def containsToDialect(self, writer):
         raise Exception("What?")
+
+    def filteredToDialect(self, writer, source):
+        raise Exception("What?")
