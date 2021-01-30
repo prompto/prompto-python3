dc5517eb2dc3654a42c66d313e11f9251e94220a
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-01-30 17:32:17 +0800

    more tests

---
+++ Python3-Core/src/test/prompto/translate/omo/TestProblems.py
@@ -0,0 +1,11 @@
+from prompto.parser.o.BaseOParserTest import BaseOParserTest
+
+class TestProblems(BaseOParserTest):
+    
+    def setUp(self):
+        super(type(self), self).setUp()
+    
+    def testAbstract(self):
+        self.compareResourceOMO("problems/abstract.poc")
+
+
