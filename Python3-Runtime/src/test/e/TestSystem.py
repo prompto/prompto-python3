3c2cb620921f24a04717d0f075ca9e93ceaa5652
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-06-02 16:18:53 +0200

    add sysGetEnvironmentVariable

---
+++ Python3-Runtime/src/test/e/TestSystem.py
@@ -0,0 +1,19 @@
+from prompto.parser.e.BaseEParserTest import BaseEParserTest
+from prompto.runtime.utils.Out import Out
+
+class TestSystem(BaseEParserTest):
+    
+    def setUp(self):
+        super(type(self), self).setUp()
+        Out.init()
+        self.coreContext = None
+        self.loadDependency("system")
+        self.loadDependency("core")
+    
+    def tearDown(self):
+        Out.restore()
+
+    def testSystem(self):
+        self.runTests("system/system.pec")
+
+
