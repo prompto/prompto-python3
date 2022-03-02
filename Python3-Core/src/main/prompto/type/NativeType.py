7e3deaaa12c096a28fd0f16ea68c07878489af4b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-03-02 14:16:48 +0100

    fix failing tests

--- Python3-Core/src/main/prompto/type/NativeType.py
+++ Python3-Core/src/main/prompto/type/NativeType.py
@@ -1,7 +1,4 @@
-from prompto.type.AnyType import AnyType
 from prompto.type.BaseType import BaseType
-from prompto.type.MissingType import MissingType
-
 
 class NativeType ( BaseType ):
 
@@ -54,6 +51,8 @@ class NativeType ( BaseType ):
 
 
     def isMoreSpecificThan(self, context, other):
+        from prompto.type.AnyType import AnyType
+        from prompto.type.MissingType import MissingType
         return other == MissingType.instance or other == AnyType.instance
 
 
