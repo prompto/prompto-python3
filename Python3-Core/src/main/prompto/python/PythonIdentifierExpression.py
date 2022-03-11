88f52fd74d257598dcb8e8a4d48e3062bbce312e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2022-03-11 11:38:53 +0100

    improve native null result handling

--- Python3-Core/src/main/prompto/python/PythonIdentifierExpression.py
+++ Python3-Core/src/main/prompto/python/PythonIdentifierExpression.py
@@ -11,13 +11,13 @@ class PythonIdentifierExpression(PythonExpression):
         parts = ids.split("\\.")
         result = None
         for part in parts:
-            result = PythonIdentifierExpression(parent=result, identifier=part)
+            result = PythonIdentifierExpression(parent=result, name=part)
         return result
 
 
-    def __init__(self, identifier, parent=None):
+    def __init__(self, name, parent=None):
         self.parent = parent
-        self.identifier = identifier
+        self.name = name
 
 
     def getParent(self):
@@ -25,21 +25,21 @@ class PythonIdentifierExpression(PythonExpression):
 
 
     def getIdentifier(self):
-        return self.identifier
+        return self.name
 
 
     def __str__(self):
         if self.parent == None:
-            return self.identifier
+            return self.name
         else:
-            return str(self.parent) + '.' + self.identifier
+            return str(self.parent) + '.' + self.name
 
 
     def toDialect(self, writer):
         if self.parent is not None:
             self.parent.toDialect(writer)
             writer.append(".")
-        writer.append(self.identifier)
+        writer.append(self.name)
 
 
     def interpret(self, context, module):
@@ -66,9 +66,9 @@ class PythonIdentifierExpression(PythonExpression):
 
 
     def interpret_prompto(self, context):
-        if self.identifier=="$context":
+        if self.name== "$context":
             return context
-        elif self.identifier == "$store":
+        elif self.name == "$store":
             return DataStore.instance
         else:
             return None
@@ -76,7 +76,7 @@ class PythonIdentifierExpression(PythonExpression):
 
     def interpret_global(self):
         try:
-            return eval(self.identifier)
+            return eval(self.name)
         except:
             return None
 
@@ -87,7 +87,7 @@ class PythonIdentifierExpression(PythonExpression):
         else:
             try:
                 m = module.resolve()
-                o = m.__dict__.get(self.identifier, None)
+                o = m.__dict__.get(self.name, None)
                 if o is None:
                     return m
                 else:
@@ -97,10 +97,13 @@ class PythonIdentifierExpression(PythonExpression):
 
 
     def interpret_instance(self, context):
-        try:
-            return context.getValue(self.identifier)
-        except:
+        if self.name == "None":
             return None
+        else:
+            try:
+                return context.getValue(self.name)
+            except:
+                return None
 
 
     def interpret_child(self, context, module):
@@ -112,4 +115,4 @@ class PythonIdentifierExpression(PythonExpression):
     def interpret_field(self, o):
         if isinstance(o, ModuleType):
             o = o.__dict__
-        return o.get(self.identifier, None)
+        return o.get(self.name, None)
